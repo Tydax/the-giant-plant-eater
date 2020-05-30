53618c322b34a9790030c736568c2271c331b500
Armand Bour <tydax@protonmail.ch>
2020-05-30 20:08:25 +0100

    💄  Add some orange touches

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,18 +14,18 @@ export default function Header(props) {
         aria-label="main navigation"
       >
         <Link to="/">
-          <h1>{props.title}</h1>
+          <h1 className={headerStyles.header__websitename}>{props.title}</h1>
         </Link>
-        <div>
-          <h1>
+        <ul className={headerStyles.header__menu}>
+          <li>
             <Link
               to={props.page === 'info' ? '/' : '/info'}
               activeClassName={headerStyles.navItemActive}
             >
               {props.page === 'info' ? 'close' : 'info'}
             </Link>
-          </h1>
-        </div>
+          </li>
+        </ul>
       </nav>
     </header>
   )
